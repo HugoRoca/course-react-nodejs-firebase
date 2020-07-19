51a8a5884fe28e2beceb3ca27b65b630842a682c
HugoRoca <hugo.rock20@hotmail.com>
2020-07-18 23:46:20 -0500

    configure navbar

--- inmobiliaria-app/src/App.js
+++ inmobiliaria-app/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
+
 import PropertyList from "./components/ListaInmuebles";
+import AppNavbar from "./components/layout/AppNavbar";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import theme from "./theme/theme";
+import Grid from "@material-ui/core/Grid";
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        <PropertyList></PropertyList>
-      </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <AppNavbar />
+          <Grid container>
+            <Switch>
+              <Route path='/' exact component={PropertyList}></Route>
+            </Switch>
+          </Grid>
+        </MuiThemeProvider>
+      </Router>
     );
   }
 }

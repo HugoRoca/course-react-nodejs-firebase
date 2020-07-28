52c92ebc4d46d2e64d6b5ba410cd6c656eb967bc
HugoRoca <hugo.rock20@hotmail.com>
2020-07-27 22:37:20 -0500

    save data on firebase

--- inmobiliaria-app/src/components/views/NewProperty/NewProperty.js
+++ inmobiliaria-app/src/components/views/NewProperty/NewProperty.js
@@ -11,19 +11,42 @@ import {
 } from "@material-ui/core";
 import { style } from "./NewProperty.css";
 import { HouseOutlined } from "@material-ui/icons";
+import { consumerFirebase } from "../../../server";
+import { openMessage } from "../../../session/actions/snackBar.action";
 
-export default class NewProperty extends Component {
+class NewProperty extends Component {
   state = {
-    property: {}
-  }
+    property: {
+      direction: "",
+      city: "",
+      country: "",
+      description: "",
+      description_in: "",
+    },
+  };
 
   onChange = (e) => {
-    // TODO: create clone
     let property = Object.assign({}, this.state.property);
     property[e.target.name] = e.target.value;
     this.setState({
       property,
     });
+  };
+
+  save = () => {
+    const { property } = this.state;
+    this.props.firebase.db
+      .collection("Properties")
+      .add(property)
+      .then((r) => {
+        this.props.history.push("");
+      })
+      .catch((err) => {
+        openMessage({
+          open: true,
+          message: err.message,
+        });
+      });
   };
 
   render() {
@@ -40,24 +63,74 @@ export default class NewProperty extends Component {
               </Breadcrumbs>
             </Grid>
             <Grid item xs={12} md={12}>
-              <TextField name="direction" label="Direction" variant="outlined" fullWidth autoComplete="off" />
+              <TextField
+                name="direction"
+                label="Direction"
+                variant="outlined"
+                fullWidth
+                autoComplete="off"
+                value={this.state.property.direction}
+                onChange={this.onChange}
+              />
             </Grid>
             <Grid item xs={12} md={6}>
-              <TextField name="city" label="City" variant="outlined" fullWidth autoComplete="off"/>
+              <TextField
+                name="city"
+                label="City"
+                variant="outlined"
+                fullWidth
+                autoComplete="off"
+                value={this.state.property.city}
+                onChange={this.onChange}
+              />
             </Grid>
             <Grid item xs={12} md={6}>
-              <TextField name="country" label="Country" variant="outlined" fullWidth autoComplete="off" />
+              <TextField
+                name="country"
+                label="Country"
+                variant="outlined"
+                fullWidth
+                autoComplete="off"
+                value={this.state.property.country}
+                onChange={this.onChange}
+              />
             </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField name="description" label="Description Property" variant="outlined" fullWidth multiline/>
+            <Grid item xs={12} md={12}>
+              <TextField
+                name="description"
+                label="Description Property"
+                variant="outlined"
+                fullWidth
+                multiline
+                value={this.state.property.description}
+                onChange={this.onChange}
+              />
             </Grid>
-            <Grid item xs={12} md={6}>
-              <TextField name="descriptionIn" label="Description Interior Property" variant="outlined" fullWidth multiline/>
+            <Grid item xs={12} md={12}>
+              <TextField
+                name="description_in"
+                label="Description Interior Property"
+                variant="outlined"
+                fullWidth
+                multiline
+                value={this.state.property.description_in}
+                onChange={this.onChange}
+              />
             </Grid>
           </Grid>
           <Grid container justify="center">
             <Grid item xs={12} md={6}>
-              <Button type="button" variant="contained" fullWidth size="large" color="primary" style={style.submit}>Save</Button>
+              <Button
+                type="button"
+                variant="contained"
+                fullWidth
+                size="large"
+                color="primary"
+                style={style.submit}
+                onClick={this.save}
+              >
+                Save
+              </Button>
             </Grid>
           </Grid>
         </Paper>
@@ -65,3 +138,5 @@ export default class NewProperty extends Component {
     );
   }
 }
+
+export default consumerFirebase(NewProperty);

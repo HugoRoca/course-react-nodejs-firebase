917986405f1a508705720ffd542c076bb4776f1d
HugoRoca <hugo.rock20@hotmail.com>
2020-08-05 22:51:36 -0500

    add configuration for redux

--- inmobiliaria-app/src/App.js
+++ inmobiliaria-app/src/App.js
@@ -4,13 +4,13 @@ import "./App.css";
 
 // TODO Components
 import PropertyList from "./components/views/ListProperty/ListProperty";
-import NewProperty from './components/views/NewProperty/NewProperty'
-import EditProperty from './components/views/EditProperty/EditProperty'
+import NewProperty from "./components/views/NewProperty/NewProperty";
+import EditProperty from "./components/views/EditProperty/EditProperty";
 import AppNavbar from "./components/Layout/AppNavBar/AppNavbar";
 import SignUp from "./components/Security/Signup/Signup";
 import SignIn from "./components/Security/Signin/Signin";
-import SignInPhone from './components/Security/SigninPhone/SigninPhone'
-import Profile from './components/Security/Profile/Profile'
+import SignInPhone from "./components/Security/SigninPhone/SigninPhone";
+import Profile from "./components/Security/Profile/Profile";
 import { FirebaseContext } from "./server";
 import RouteAuth from "./components/Security/RouteAuth/RouteAuth";
 
@@ -21,6 +21,9 @@ import Grid from "@material-ui/core/Grid";
 
 // TODO Sessions
 import { useStateValue } from "./session/store";
+
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 function App(props) {
   let firebase = React.useContext(FirebaseContext);
@@ -34,62 +37,68 @@ function App(props) {
   });
 
   return authenticationStarted !== false ? (
-    <React.Fragment>
-      <Snackbar
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        autoHideDuration={3000}
-        onClose={() =>
-          dispatch({
-            type: "OPEN_SNACKBAR",
-            snackBar: {
-              open: false,
-              message: "",
-            },
-          })
-        }
-        ContentProps={{ "aria-describedby": "message-id" }}
-        message={
-          <span id="message-id">{snackBar ? snackBar.message : ""}</span>
-        }
-        open={snackBar ? snackBar.open : false}
-      ></Snackbar>
-      <Router>
-        <MuiThemeProvider theme={theme}>
-          <AppNavbar />
-          <Grid container>
-            <Switch>
-              <RouteAuth
-                exact
-                path="/"
-                component={PropertyList}
-                authenticateFirebase={firebase.auth.currentUser}
-              />
-              <RouteAuth
-                exact
-                path="/profile"
-                component={Profile}
-                authenticateFirebase={firebase.auth.currentUser}
-              />
-              <RouteAuth
-                exact
-                path="/property/new"
-                component={NewProperty}
-                authenticateFirebase={firebase.auth.currentUser}
-              />
-              <RouteAuth
-                exact
-                path="/property/edit/:id"
-                component={EditProperty}
-                authenticateFirebase={firebase.auth.currentUser}
-              />
-              <Route path="/signup" exact component={SignUp}></Route>
-              <Route path="/signin" exact component={SignIn}></Route>
-              <Route path="/signin/phone" exact component={SignInPhone}></Route>
-            </Switch>
-          </Grid>
-        </MuiThemeProvider>
-      </Router>
-    </React.Fragment>
+    <Provider store={store}>
+      <React.Fragment>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          autoHideDuration={3000}
+          onClose={() =>
+            dispatch({
+              type: "OPEN_SNACKBAR",
+              snackBar: {
+                open: false,
+                message: "",
+              },
+            })
+          }
+          ContentProps={{ "aria-describedby": "message-id" }}
+          message={
+            <span id="message-id">{snackBar ? snackBar.message : ""}</span>
+          }
+          open={snackBar ? snackBar.open : false}
+        ></Snackbar>
+        <Router>
+          <MuiThemeProvider theme={theme}>
+            <AppNavbar />
+            <Grid container>
+              <Switch>
+                <RouteAuth
+                  exact
+                  path="/"
+                  component={PropertyList}
+                  authenticateFirebase={firebase.auth.currentUser}
+                />
+                <RouteAuth
+                  exact
+                  path="/profile"
+                  component={Profile}
+                  authenticateFirebase={firebase.auth.currentUser}
+                />
+                <RouteAuth
+                  exact
+                  path="/property/new"
+                  component={NewProperty}
+                  authenticateFirebase={firebase.auth.currentUser}
+                />
+                <RouteAuth
+                  exact
+                  path="/property/edit/:id"
+                  component={EditProperty}
+                  authenticateFirebase={firebase.auth.currentUser}
+                />
+                <Route path="/signup" exact component={SignUp}></Route>
+                <Route path="/signin" exact component={SignIn}></Route>
+                <Route
+                  path="/signin/phone"
+                  exact
+                  component={SignInPhone}
+                ></Route>
+              </Switch>
+            </Grid>
+          </MuiThemeProvider>
+        </Router>
+      </React.Fragment>
+    </Provider>
   ) : null;
 }
 
---
+++ inmobiliaria-app/src/redux/actions/user.action.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export const getUsersApp = (dispatch) => {
+  return new Promise(async (resolve, reject) => {
+    const dataRest = await axios.get(
+      `https://us-central1-storied-smile-283703.cloudfunctions.net/usersList/list`
+    );
+    dispatch({
+      type: "LIST_USERS",
+      payload: dataRest.data,
+    });
+    resolve();
+  });
+};
+
+export const updateRoles = (dispatch, user) => {
+  return new Promise(async (resolve, reject) => {
+    const dataRest = await axios.post(
+      `https://us-central1-storied-smile-283703.cloudfunctions.net/usersCrud`,
+      user
+    );
+    dispatch({
+      type: "UPDATE_ROLES",
+      payload: dataRest.data,
+    });
+    resolve();
+  });
+};
